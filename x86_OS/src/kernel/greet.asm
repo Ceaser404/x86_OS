840f99ba53eef4b29e5c9fc364abf0e1cdba5090
Ceaser404 <119560473+Ceaser404@users.noreply.github.com>
2023-12-03 19:23:55 -0600

    Add files via upload

---
+++ x86_OS/src/kernel/greet.asm
@@ -0,0 +1,43 @@
+start:
+    ; print hello world message
+
+    mov si, msg_hello
+    call puts
+
+.halt:
+    cli
+    hlt
+
+
+;
+; Prints a string to the screen
+; Params:
+;   - ds:si points to string
+;
+puts:
+    ; save registers we will modify
+    push si
+    push ax
+    push bx
+
+.loop:
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
+    jz .done            ; jump if equal to (Jz or Je)
+
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
+    int 0x10
+
+    jmp .loop
+
+.done:
+    pop bx
+    pop ax
+    pop si    
+    
+
+msg_hello: db 'Hello world from Kernel!', ENDL, 0
+
+
+
